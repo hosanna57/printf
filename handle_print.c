06fc33c8c25877baaead2585928ab4da0ac08bf1
hosanna57 <hosannacchigbu@gmail.com>
2023-03-27 21:45:34 -0700

    handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,20 +1,21 @@
 #include "main.h"
+
 /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
+ * @ind: Index of the current character in fmt.
  * @list: List of arguments to be printed.
- * @ind: ind.
  * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: 1 or 2;
+ * @width: Width specifier.
+ * @precision: Precision specification.
+ * @size: Size specifier.
+ * Return: Number of characters printed, or -1 on error.
  */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[], 
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i, unknow_len = 0, printed_chars = -1;
+	int i, unknown_len = 0, printed_chars = 0;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -22,28 +23,32 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
-	if (fmt_types[i].fmt == '\0')
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 	{
-		if (fmt[*ind] == '\0')
-			return (-1);
-		unknow_len += write(1, "%%", 1);
-		if (fmt[*ind - 1] == ' ')
-			unknow_len += write(1, " ", 1);
-		else if (width)
-		{
-			--(*ind);
-			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-				--(*ind);
-			if (fmt[*ind] == ' ')
-				--(*ind);
-			return (1);
-		}
-		unknow_len += write(1, &fmt[*ind], 1);
-		return (unknow_len);
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return fmt_types[i].fn(list, buffer, flags, width, precision, size);
 	}
-	return (printed_chars);
+
+	if (fmt[*ind] == '\0')
+		return -1; /* End of string */
+
+	if (fmt[*ind] == ' ')
+		unknown_len += write(1, " ", 1);
+
+	if (width)
+	{
+		/* Skip back to beginning of specifier */
+		while (*ind > 0 && fmt[*ind - 1] != '%')
+			(*ind)--;
+		if (*ind > 0 && fmt[*ind - 1] == '%')
+			(*ind)--; /* Back up one more if we landed on a % */
+		return 1; /* Handled specifier width */
+	}
+
+	/* Unrecognized specifier - print % and specifier */
+	unknown_len += write(1, "%%", 1);
+	unknown_len += write(1, &fmt[*ind], 1);
+
+	return unknown_len;
 }
